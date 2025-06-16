ff2e04d2783b10f883886e63df52554c28d354b4
roychandu <cr717753@gmail.com>
2025-06-16 13:03:20 +0530

    animation controller added

--- lib/view/world_wise_screen.dart
+++ lib/view/world_wise_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class WorldWiseScreen extends StatefulWidget {
@@ -7,7 +9,34 @@ class WorldWiseScreen extends StatefulWidget {
   State<WorldWiseScreen> createState() => _WorldWiseScreenState();
 }
 
-class _WorldWiseScreenState extends State<WorldWiseScreen> {
+class _WorldWiseScreenState extends State<WorldWiseScreen>
+    with TickerProviderStateMixin {
+  // AnimationController _controller;
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Timer() for duration of animation
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WorldWiseScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold());
