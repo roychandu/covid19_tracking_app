810058aabdc5b56d47ee7b84a4c9ae2dbbad13e1
roychandu <cr717753@gmail.com>
2025-06-17 10:32:18 +0530

    add api inside the api_url.dart file

---
+++ lib/Services/Utilities/api_url.dart
@@ -0,0 +1,6 @@
+class Appurl {
+  static const String baseurl = 'https://disease.sh/v3/covid-19/';
+
+  static const String worldwiseApi = '${baseurl}all';
+  static const String countriesApi = '${baseurl}countries';
+}
