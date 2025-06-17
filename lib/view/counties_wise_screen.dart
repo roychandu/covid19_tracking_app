3e9874cac0937ba5a361171c260f733fbf67b3f6
roychandu <cr717753@gmail.com>
2025-06-17 13:41:25 +0530

    add searching input field

--- lib/view/counties_wise_screen.dart
+++ lib/view/counties_wise_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CountiesWiseScreen extends StatefulWidget {
   const CountiesWiseScreen({super.key});
@@ -8,8 +9,33 @@ class CountiesWiseScreen extends StatefulWidget {
 }
 
 class _CountiesWiseScreenState extends State<CountiesWiseScreen> {
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.search),
+                  hintText: 'Search with country name',
+                  contentPadding: EdgeInsets.only(left: 20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
