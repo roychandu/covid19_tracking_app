7ca7f4352f322247519536f775fd3b3fdc00f04b
roychandu <cr717753@gmail.com>
2025-06-17 14:52:43 +0530

    add shimmer effect and all necessary data fetched

--- lib/view/counties_wise_screen.dart
+++ lib/view/counties_wise_screen.dart
@@ -1,6 +1,7 @@
 import 'package:covid19_tracking_app/Services/state_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CountiesWiseScreen extends StatefulWidget {
   const CountiesWiseScreen({super.key});
@@ -35,7 +36,63 @@ class _CountiesWiseScreenState extends State<CountiesWiseScreen> {
                 ),
               ),
             ),
-            FutureBuilder(future: services, builder: (context, snapshot) {}),
+            Expanded(
+              child: FutureBuilder(
+                future: services.countryDataFunc(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Image(
+                            width: 50,
+                            height: 50,
+                            image: NetworkImage(
+                              snapshot.data?[index]['countryInfo']['flag'],
+                            ),
+                          ),
+                          title: Text(
+                            snapshot.data![index]['country'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            'Cases : ${snapshot.data![index]['cases']}',
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.grey.shade100,
+                          highlightColor: Colors.grey.shade700,
+                          child: ListTile(
+                            leading: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.white,
+                            ),
+                            title: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.white,
+                            ),
+                            subtitle: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
